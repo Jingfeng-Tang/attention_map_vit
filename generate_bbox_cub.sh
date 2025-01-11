ee9a715663b12b87ff302e257aef6bfcd4fc30c7
tjf <tjf_1105@163.com>
2025-01-11 22:04:58 +0800

    first commit

---
+++ generate_bbox_cub.sh
@@ -0,0 +1,9 @@
+python main.py --model vit_small_patch16_224_attmap \
+                --batch-size 1 \
+                --data-set CUB \
+                --img-list cub \
+                --data-path /data/c425/tjf/datasets/CUB_200_2011/ \
+                --layer-index 12 \
+                --output_dir results_vit_small \
+                --gen_bounding_boxes \
+                --ckpt /data/c425/tjf/attention_map_vit/results_vit_small/2025-01-07-20-24-19_ok/checkpoint_best.pth
---
+++ train_val_cub.sh
@@ -0,0 +1,21 @@
+python main.py --model vit_small_patch16_224_attmap \
+                --batch-size 128 \
+                --epochs 60 \
+                --data-set CUB \
+                --img-list cub \
+                --data-path /data/c425/tjf/datasets/CUB_200_2011/ \
+                --layer-index 12 \
+                --output_dir results_vit_small \
+                --finetune /data/c425/tjf/attention_map_vit/pretrained/S_16-i21k-300ep-lr_0.001-aug_light1-wd_0.03-do_0.0-sd_0.0--imagenet2012-steps_20k-lr_0.03-res_224.npz
+
+
+
+
+python main.py --model vit_small_patch16_224.augreg_in21k_ft_in1k \
+                --batch-size 128 \
+                --data-set CUB \
+                --img-list cub \
+                --data-path /data/c425/tjf/datasets/CUB_200_2011/ \
+                --layer-index 12 \
+                --output_dir results_vit_small \
+                --finetune /data/c425/tjf/attention_map_vit/pretrained/S_16-i21k-300ep-lr_0.001-aug_light1-wd_0.03-do_0.0-sd_0.0--imagenet2012-steps_20k-lr_0.03-res_224.npz
